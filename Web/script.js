b6891a05fe93975c2fad491c43961317af2190c4
Tina <jto2554@gmail.com>
2026-07-27 11:21:32 +0800

    修改

--- Web/script.js
+++ Web/script.js
@@ -72,10 +72,8 @@ slideshow.addEventListener("mouseleave",function(){
 
 // -------------------------------------------------------------------
 
-// Get the modal
 var modal = document.getElementById('id01');
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
